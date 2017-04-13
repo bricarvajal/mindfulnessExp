41fdb57b9bea05cce1408793169baf8ffa3dc337
luisdramirez <ldr285@nyu.edu>
2017-04-12 20:27:47 -0400

    audio changes.

--- Mindfulness_Memory_Task_Exp.m
+++ Mindfulness_Memory_Task_Exp.m
@@ -7,12 +7,15 @@ subjGroup = ''; %Options: 'experimental' 'control'
 switch subjGroup
     case 'experimental'
         % loads audio, 10 minute mindfulness audio (black screen)
-        audioFileName = 'audiofile';
-        [y,Fs] = audioread(audioFileName);
-        InitializePsychSound(1)
-        PsychPortAduio('Open', [], [], 2, Fs, 1)
-        
-        
+        wavfilename = 'stimuli/DW_Breath_Sound_Body_Meditation.wav';
+        InitializePsychSound;
+        [y, freq] = psychwavread(wavfilename);
+        wavedata = y';
+        nrchannels = size(wavedata,1); % Number of rows == number of channels.
+        pahandle = PsychPortAudio('Open', [], [], 0, freq, nrchannels);
+        soundAmp = 1;
+        % Fill the audio playback buffer with the audio data 'wavedata':
+        PsychPortAudio('FillBuffer', pahandle, wavedata);
     case 'control'
         % no audio; 10 minute block
         
@@ -55,30 +58,39 @@ retryText = ['You did not enter ' num2str(param.numDig) '.\n Please enter '...
 
 % Draw text in the middle of the screen in Courier in white
 Screen('TextSize', window, 50);
-Screen('TextFont', window, 'Courier');
+Screen('TextFont', window, 'Arial');
 
-for iTrial = 1:param.numTrials
-    for iNum = 1:param.numDig
-        curr_num = num2str(trials.numSeq(iTrial, iNum));
-        % Draw number sequence
-        DrawFormattedText(window, curr_num, 'center', 'center', black);
-        % Flip to the screen
-        Screen('Flip', window);
-        pause(param.seqDur);      
+for iBlock = 1:param.numBlocks
+    if iBlock == param.numBlocks
+        % Start audio playback for 'repetitions' repetitions of the sound data,
+        % start it immediately (0) and wait for the playback to start, return onset
+        % timestamp.
+        t1 = PsychPortAudio('Start', pahandle, 0, 0, 1);        
+        [startTime endPositionSecs xruns estStopTime] = PsychPortAudio('Stop', y*soundAmp, 1);
     end
-    if iNum == param.numDig
-        DrawFormattedText(window, respText, 'center', 'center', black);
-    %   [keyIsDown, secs, keyCode, deltaSecs] = KbCheck([deviceNumber]);
-        Screen('Flip', window);
-        response = input('user input: ', 's');
-        response = response - '0';
-        while length(response) ~= param.numDig
-            DrawFormattedText(window, retryText, 'center', 'center', black);
+    for iTrial = 1:param.numTrials
+        for iNum = 1:param.numDig
+            curr_num = num2str(trials.numSeq(iTrial, iNum));
+            % Draw number sequence
+            DrawFormattedText(window, curr_num, 'center', 'center', black);
+            % Flip to the screen
             Screen('Flip', window);
-            response = input('retry user input: ', 's');
-            response = response - '0';
+            pause(param.seqDur);      
         end
-        trials.responses(iTrial,:) = response;
+        if iNum == param.numDig
+            DrawFormattedText(window, respText, 'center', 'center', black);
+        %   [keyIsDown, secs, keyCode, deltaSecs] = KbCheck([deviceNumber]);
+            Screen('Flip', window);
+            response = input('user input: ', 's');
+            response = response - '0';
+            while length(response) ~= param.numDig
+                DrawFormattedText(window, retryText, 'center', 'center', black);
+                Screen('Flip', window);
+                response = input('retry user input: ', 's');
+                response = response - '0';
+            end
+            trials.responses(iTrial,:) = response;
+        end
     end
 end
 
@@ -86,6 +98,7 @@ save(['data/MMT_' num2str(subj) '_' num2str(run)], 'trials', 'param')
 
 DrawFormattedText(window, endText, 'center', 'center', black);
 Screen('Flip', window);
+PsychPortAudio('Close' [, pahandle]);
 KbStrokeWait;
 sca;
 
